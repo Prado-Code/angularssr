6f0feb43c7126c51e5f04586592e617ef377e032
Prado Ricardo De Leon Nolasco <Prado.code@outlook.com>
2025-01-13 14:46:12 -0400

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { ForthComponent } from './forth/forth.component';
 
 export const routes: Routes = [
 {path: "first", component: FirstComponent},
 {path: "second", component: SecondComponent},
+{path: "third/:id", component:ThirdComponent },
+{path: "fourth", component:ForthComponent },
+
 ];
---
+++ src/app/forth/forth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forth',
+  standalone: true,
+  imports: [],
+  templateUrl: './forth.component.html',
+  styleUrl: './forth.component.css'
+})
+export class ForthComponent {
+
+}
